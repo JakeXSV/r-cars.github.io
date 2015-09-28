d5827cdc310881e7416c8523fc2faaec0e9eaf64
Katie Gilbert <ktagilbert@gmail.com>
2015-09-28 16:24:20 -0700

    pull request changes

--- js/splashController.js
+++ js/splashController.js
@@ -15,8 +15,7 @@ $(window).scroll(
             if (!articleWaypointsInitialized && isMobile.any) {
                 // Scroll down first article
                 new Waypoint({
-                  //this was driving me crazy - changed from id article 2 to first post-title
-                    element: document.getElementsByClassName(".post-title").first();,
+                    element: document.getElementsByClassName("article2"),
                     handler: function(direction) {
                         if (isSplashMinimized) {
                             $(splashSelector).css("background-image", "url(images/posts/2015/8/15ChallengerRTScat/headerf.jpg)");
