f337d98c80971d07c92a114404f74339206e8f1b
Jake Prather <JakeHP@Zoho.com>
2015-10-19 19:12:51 -0500

    Search func. New search json.

--- js/searchBar.js
+++ js/searchBar.js
@@ -1,33 +1,35 @@
-//show search text area on click of search icon
+var searchData = {};
+$.get("/search.json", function(data) {
+    console.log(data);
+    searchData = data;
+});
 $('.genericon-search').on('click', function(e) {
     $('.search-text').animate({
         width: 'toggle'
     }, '200');;
 })
-//search function
-var $articleTitles = $('.post-title');
-var searchTerm = "";
-function search() {
-    $articleTitles.each(function(i, elem) {
-        if ($(elem).text().toUpperCase().indexOf(searchTerm) === -1) {
-            $(elem).closest('article').hide();
-        } else {
-            $(elem).closest('article').show();
-        }
-    });
-    if ($('article:hidden').length === $articleTitles.length) {
-        $('#no-results').show();
-    }
-}
+var articleTitles = $('.post-title');
+var searchTerm = '';
 $(".search-text").on('keydown', function(e) {
     if (e.keyCode === 8) {
         if (searchTerm.length > 0) {
             searchTerm = searchTerm.substring(0, searchTerm.length - 1);
-            $('#no-results').hide();
-            search();
+            searchJson(searchTerm, searchData);
+        } else {
+            // display all
         }
     } else {
         searchTerm += String.fromCharCode(e.keyCode);
-        search();
+        searchJson(searchTerm, searchData);
     }
 });
+function searchJson(text, dataSet) {
+    text = text.toLowerCase();
+    var foundArticles = [];
+    for (var prop in dataSet) {
+        if (prop.indexOf(text) > -1) {
+            foundArticles.push(dataSet[prop]);
+        }
+    }
+    console.log(foundArticles);
+}
